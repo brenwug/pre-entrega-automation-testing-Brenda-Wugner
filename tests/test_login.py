746979f36ece55d552ad51d61b2e79ae7104a321
Brenda <brendadianawugner@gmail.com>
2025-10-25 11:30:13 -0300

    Organización del proyecto, se agrega espera explicita y validación de titulo Products

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+import os
 
 def test_login():
    
@@ -11,19 +13,24 @@ def test_login():
     try:
         # Ir al sitio
         driver.get("https://www.saucedemo.com/")
-        time.sleep(1)
 
-        # Completar login
+        # Validar login
         driver.find_element(By.ID, "user-name").send_keys("standard_user")
         driver.find_element(By.ID, "password").send_keys("secret_sauce")
         driver.find_element(By.ID, "login-button").click()
 
-        time.sleep(2)
+        WebDriverWait(driver, 10).until(EC.url_contains("/inventory.html"))
 
-        # Validar resultado
+        # Validar resultado /inventory.html
         assert "/inventory.html" in driver.current_url, "No se redirigió correctamente al inventario"
         driver.save_screenshot("login_exitoso.png")
         print("Login exitoso y validado correctamente")
+
+        titulo = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "title"))
+        ).text
+        assert titulo == "Products", f"El título esperado era Products pero se obtuvo '{titulo}"
+        print(f"El título en la página es '{titulo}'")
 
     except Exception as e:
         print(f"Error en test login: {e}")
